5bc25b194b64a24dae8950457f270e83a4e38063
JophiArcana <wentinn.liao@gmail.com>
2023-04-22 20:33:01 -0700

    hparams update

--- dl_pytorch/hparams.py
+++ dl_pytorch/hparams.py
@@ -7,7 +7,10 @@ HP = Namespace(
     lr_decay=0.99,
     optim_type="adam",
     l2_reg=0.0,
-    epochs=5,
-    do_batchnorm=False,
-    p_dropout=0.0,
+    epochs=10,
+    hidden_channels=128,
+    num_residual_layers=2,
+    residual_hidden_channels=32,
+    num_embeddings=512,
+    embedding_dim=64
 )
